0213f776742eaa7724103fbef89cdcea13951ece
Bal Kishan <bkunsolved@gmail.com>
2024-10-18 12:46:49 +0530

    update ui

--- app/_components/SideBar.tsx
+++ app/_components/SideBar.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function SideBar() {
   return (
-    <div className="border-r-[1px] border-gray-300 hidden md:block">
+    <div className="border-r-[1px] border-gray-300 hidden md:block ">
       <div className="flex items-center justify-center gap-x-2 p-4 pr-10">
         <Image src={"/images/logo.png"} width={40} height={40} alt="Logo" />
         <span className="text-orange-400 font-extrabold text-3xl font-sans tracking-wide self-end">
           omehgul
         </span>
       </div>
-      <div className="flex flex-col items-start p-4 pr-10 gap-y-4">
+      <div className="flex flex-col items-start p-4 pr-10 gap-y-4 h-[90vh] overflow-y-scroll">
         <div className="font-semibold text-orange-400">Now</div>
         <div className="flex flex-col">
           <div className="flex flex-col hover:bg-orange-300 px-4 py-2 rounded-md">
