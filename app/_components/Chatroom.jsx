fe5e0aaa9eac4d5c671975cfbe0940320f4f70ab
Bal Kishan <bkunsolved@gmail.com>
2024-10-28 18:47:19 +0530

    add next room logic

--- app/_components/Chatroom.jsx
+++ app/_components/Chatroom.jsx
@@ -3,10 +3,15 @@ import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 import VideoBox from "./VideoBox";
 import MessageBox from "./MessageBox";
+import { useSocket } from "./SocketProvider";
+import { useRouter } from "next/navigation";
+import { RouteMatcher } from "next/dist/server/future/route-matchers/route-matcher";
 
 function Chatroom({ isVideo, audioTrack, videoTrack }) {
   console.log("Line no. 9");
   console.log(videoTrack);
+  const { socket } = useSocket();
+  const router = useRouter();
   return (
     <>
       {/* <NavBar /> */}
@@ -18,6 +23,16 @@ function Chatroom({ isVideo, audioTrack, videoTrack }) {
           videoTrack={videoTrack}
         />
         <MessageBox isVideo={isVideo} />
+        <form
+          onSubmit={() => {
+            // socket.disconnect();
+            router.refresh("/");
+          }}
+        >
+          <button className="bg-orange-300 text-white px-8 py-2 fixed top-4 right-10 rounded-md">
+            Next
+          </button>
+        </form>
       </div>
     </>
   );
